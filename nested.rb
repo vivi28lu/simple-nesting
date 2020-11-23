8b5203fde801a1939fed651052add005264eb691
viviana <vivi28lu@gmail.com>
2020-11-22 21:52:49 -0500

    commit tree d7ebf5a69586fa83e91f225295c356486c5287a2

--- nested.rb
+++ nested.rb
@@ -15,12 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay]
 end
 
 def dennis_ritchies_language
